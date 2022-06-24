72c541aa9921d68d242349f61a1fd0179d6e46ce
sephyrot <sephyrot@hanmail.net>
2022-06-24 12:00:43 +0900

    Django board model, write 작성

--- board/models.py
+++ board/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
+from member.models import Member
 
 # Create your models here.
+
+# on_delete : CASCADE, DO_NOTHING
+
+
+class Board(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length = 100)
+    userid = models.ForeignKey(Member, db_column='userid', on_delete = models.CASCADE)
+    regdate = models.DateTimeField(default=datetime.now)
+    views  = models.IntegerField(default=0)
+    contents = models.TextField(null=False, blank=False)
+
+    class Meta :
+        db_table = 'board' # 생성할 테이블 이름 지정
+        ordering = ['-regdate'] # 정렬기준 지정
+
+    def __str__(self):
+        return self.title  # 객체 출력시 출력형태 정의
