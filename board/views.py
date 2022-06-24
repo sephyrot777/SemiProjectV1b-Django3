33db7eff183f0064bdd824e379e40bee74762ed3
sephyrot <sephyrot@hanmail.net>
2022-06-24 15:20:22 +0900

    Django board 새글쓰기, 리스트 출력

--- board/views.py
+++ board/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from board.models import Board
+from member.models import Member
 
 # Create your views here.
+
+# 게시판 목록보기 처리
 def list(request):
-    return render(request, 'board/list.html')
+    bdlist = Board.objects.values('id','title','userid','regdate','views').order_by('-id')
 
+    context = {'bds': bdlist}
+    return render(request, 'board/list.html', context)
 
+# 게시판 본문보기 처리
 def view(request):
-    return render(request, 'board/view.html')
+    return (request, 'board/view.html')
 
 
+# 게시판 글쓰기 처리
+# get : board/write.html
+# post : 작성한 글을 디비에 저장, board/list.html로 이동
 def write(request):
-    return render(request, 'board/write.html')
+    returnPage = 'board/write.html'
+    form = ''
+    error = ''
+
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        form = request.POST.dict()
+
+        # 유효성검사 1/2/3
+
+        if not (form['title'] and form['contents']):
+            error = '제목이나 본문을 작성하세요!!'
+        else:
+            # 입력한 게시글을 Board객체에 담음
+            board = Board(title = form['title'],
+                          contents = form['contents'],
+                          # 새글을 작성한 회원에 대한 정보는
+                          # 회원테이블에 존재하는 회원번호(id)를
+                          # 조회해서 userid 속성에 아이디 저장
+                          userid = Member.objects.get(pk=form['memberid']))
+
+            board.save()    # board객체에 담은 게시글을 테이블에 저장
+
+            return redirect('/list')
+
+    context = {'form': form, 'error':error}
+    return render(request, returnPage, context)
--- member/views.py
+++ member/views.py
@@ -74,6 +74,12 @@ def login(request):
                 # 세션변수에 인증정보를 저장해 둠
                 request.session['userid'] = form['userid']
 
+                # 로그인한 사용자의 아이디도 조회해서 세션변수에 저장해 둠
+                id = Member.objects.all().filter(userid = form['userid']).values_list('id')[0][0]
+                request.session['userid_id'] = id
+                print(id)
+
+
                 return redirect('/') # index 페이지로 이동
 
             else:
